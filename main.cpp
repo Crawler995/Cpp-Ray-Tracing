da6f3166290d8216f4b951a9fd4dab6b2a50ee3c
Mr.Zhang <35191814+Crawler995@users.noreply.github.com>
2019-02-19 20:46:55 +0800

    Add progress bar

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 #include <algorithm>
 #include "libs/vector3.h"
 #include "libs/ray3.h"
@@ -7,9 +8,11 @@
 #include "libs/intersect_result.h"
 #include "libs/perspective_camera.h"
 
-#define WIDTH 800
-#define HEIGHT 600
+#define WIDTH 1920
+#define HEIGHT 1080
 #define MAX_DEPTH 500
+
+void show_progress(int index, int two_percent_pixel_num);
 
 int main(int argc, char const *argv[])
 {   
@@ -21,7 +24,7 @@ int main(int argc, char const *argv[])
         Vector3(0, 0, 0)
     );
 
-    Sphere sphere(Vector3(0, 0, -400), 100);
+    Sphere sphere(Vector3(0, 0, -400), 200);
     
     FILE *f = fopen("test.ppm", "wb");
     if(!f) {
@@ -29,6 +32,13 @@ int main(int argc, char const *argv[])
     }
     else {
         fprintf(f, "P3\n%d %d\n255\n", WIDTH, HEIGHT);
+
+        int index = 0;
+        int two_percent_pixel_num = WIDTH * HEIGHT / 50;
+        double percent = 0;
+
+        clock_t start, end;
+        start = clock();
 
         for(int i = HEIGHT - 1; i >= 0; i--) {
             for(int j = 0; j < WIDTH; j++) {
@@ -39,10 +49,34 @@ int main(int argc, char const *argv[])
 
                 int distance = 255 - std::min((res.distance / MAX_DEPTH) * 255, 255.0);
                 fprintf(f, "%d %d %d\n", distance, distance, distance);
+
+                index++;
+                show_progress(index, two_percent_pixel_num);
             }
         }
 
+        end = clock();
+        double run_time = (double)(end - start) / CLOCKS_PER_SEC;
+
+        std::cout << std::endl << "Render done! Spend " << run_time << "s." << std::endl;
+
         fclose(f);
     }
+
     return 0;
+}
+
+void show_progress(int index, int two_percent_pixel_num) {
+    if(index % two_percent_pixel_num == 0) {
+        std::cout << "\r[";
+        for(int i = 0; i < index / two_percent_pixel_num; i++) {
+            std::cout << "#";
+        }
+        for(int i = 0; i < 50 - index / two_percent_pixel_num; i++) {
+            std::cout << " ";
+        }
+        std::cout << "] ";
+        int percent = 100.0 * index / (WIDTH * HEIGHT);
+        std::cout << "[" << percent << "%]";
+    }
 }
